9b12a19c563d3cdb0f612dad84927c65ffc94ecc
Bryan Hockey <bhock@umich.edu>
2017-06-05 09:34:28 -0500

    add QueueItemBuilder

--- lib/queue_item_builder.rb
+++ lib/queue_item_builder.rb
@@ -1,5 +1,24 @@
 
 class QueueItemBuilder
-  def initialize(args); end
-  def build; end
+
+  def initialize(request)
+    @request = request
+  end
+
+  def build
+    queue_item = QueueItem.new(request: request, bag: nil)
+    if queue_item.valid?
+      FileMoveJob.perform_later(queue_item, request.upload_path, storage_location)
+    end
+  end
+
+  private
+
+  attr_reader :request
+
+  def storage_location
+    root = Rails.application.config.upload['storage_path']
+    user = request.user.username
+    File.join root, user, request.bag_id
+  end
 end
