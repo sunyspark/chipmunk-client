b99d27112baeb2d465d1b66427edb2aceb5177ac
Aaron Elkiss <aelkiss@umich.edu>
2017-06-07 12:52:54 -0400

    BagMoveJob validates incoming bags as ChipmunkBags

--- app/jobs/bag_move_job.rb
+++ app/jobs/bag_move_job.rb
@@ -1,37 +1,67 @@
 class BagMoveJob < ApplicationJob
   def perform(queue_item, src_path, dest_path)
+    @queue_item = queue_item
+    @src_path = src_path
+    @dest_path = dest_path
 
     begin
-      File.rename(src_path,dest_path)
-
-      # When a bag is just a completed request 
-      #  - start a transaction that updates the request to complete
-      #  - move the bag into place
-      #  - success: commit the transaction
-      #  - failure (exception) - transaction automatically rolls back
-      queue_item.transaction do
-        queue_item.bag = bag_type(queue_item).create!(
-          bag_id: queue_item.request.bag_id,
-          user: queue_item.user,
-          storage_location: dest_path,
-          external_id: queue_item.request.external_id,
-        )
-        queue_item.status = :done
-        queue_item.save!
+      
+      # TODO when a bag is just a completed request 
+      #  - if all validation succeeds:
+      #    - start a transaction that updates the request to complete
+      #    - move the bag into place
+      #    - success: commit the transaction
+      #    - failure (exception) - transaction automatically rolls back
+      if bag_is_valid?
+        File.rename(src_path,dest_path)
+        record_success
       end
+
     rescue => exception
-      # attempt to log
-      queue_item.error = exception.to_s
-      queue_item.status = :failed
-      queue_item.save!
-      # re-raise
+      record_error(exception.to_s)
       raise exception
     end
   end
 
   private
 
-  def bag_type(queue_item)
+  attr_accessor :queue_item, :src_path, :dest_path
+
+  def bag_is_valid?
+    bag = ChipmunkBag.new(src_path)
+    if bag.valid?
+      true
+    else
+      record_error("Error validating bag:\n" + 
+                   indent_array(bag.errors.full_messages))
+
+      false
+    end
+
+  end
+
+  def record_error(error)
+    queue_item.transaction do
+      queue_item.error = error
+      queue_item.status = :failed
+      queue_item.save!
+    end
+  end
+
+  def record_success
+    queue_item.transaction do
+      queue_item.bag = bag_type.create!(
+        bag_id: queue_item.request.bag_id,
+        user: queue_item.user,
+        storage_location: dest_path,
+        external_id: queue_item.request.external_id,
+      )
+      queue_item.status = :done
+      queue_item.save!
+    end
+  end
+
+  def bag_type
     case queue_item.request.content_type
     when :audio
       AudioBag
@@ -41,4 +71,9 @@ class BagMoveJob < ApplicationJob
       raise ArgumentError, "there has to be a better way of doing this"
     end
   end
+
+  def indent_array(array,width=2)
+    array.map { |s| ' '*width + s }.join("\n")
+  end
+  
 end
--- spec/jobs/bag_move_job_spec.rb
+++ spec/jobs/bag_move_job_spec.rb
@@ -11,50 +11,78 @@ RSpec.describe BagMoveJob do
   describe '#perform' do
     subject { described_class.perform_now(queue_item,srcpath,destpath) }
 
-    context "when the move succeeds" do
-      before(:each) do
-        allow(File).to receive(:rename).with(srcpath,destpath).and_return true
+    before(:each) do
+      allow(ChipmunkBag).to receive(:new).with(srcpath).and_return(fakebag)
+    end
+
+    context "when the bag is valid" do
+      let(:fakebag) { double('fake bag', valid?: true ) }
+
+
+      context "and the move succeeds" do
+        before(:each) do 
+          allow(File).to receive(:rename).with(srcpath,destpath).and_return true
+        end
+        it "moves the bag" do
+          expect(File).to receive(:rename).with(srcpath,destpath)
+          subject
+        end
+
+        it "updates the queue_item to status :done" do
+          subject
+          expect(queue_item.status).to eql("done")
+        end
       end
 
-      it "moves the bag" do
-        expect(File).to receive(:rename).with(srcpath,destpath)
-        subject
-      end
+      context "but the move fails" do
+        before(:each) do
+          allow(File).to receive(:rename).with(srcpath,destpath).and_raise InjectedError, 'injected error'
+        end
 
-      it "on success, updates the queue_item to status :done" do
-        subject
-        expect(queue_item.status).to eql("done")
+        it "re-raises the exception" do
+          expect { subject }.to raise_exception(InjectedError)
+        end
+
+        it "updates the queue_item to status 'failed'" do
+          begin
+            subject
+          rescue InjectedError
+          end
+
+          expect(queue_item.status).to eql("failed")
+        end
+
+        it "records the error in the queue_item" do
+
+          begin
+            subject
+          rescue InjectedError
+          end
+
+          expect(queue_item.error).to match(/injected error/)
+        end
       end
     end
 
-    context "when there is an error" do
-      before(:each) do
-        allow(File).to receive(:rename).with(srcpath,destpath).and_raise InjectedError, 'injected error'
-      end
+    context "when the bag is invalid" do
+      let(:errors) { double('errors', full_messages: ['injected error']) }
+      let(:fakebag) { double('fake bag', valid?: false, errors: errors ) }
 
-      it "re-raises the exception" do
-        expect { subject }.to raise_exception(InjectedError)
+      it "does not move the bag" do
+        expect(File).not_to receive(:rename).with(srcpath,destpath)
       end
 
       it "updates the queue_item to status 'failed'" do
-        begin
-          subject
-        rescue InjectedError
-        end
-
-        expect(queue_item.status).to eql("failed")
+        subject
+        expect(queue_item.status).to eql('failed')
       end
 
-      it "records the error in the queue_item" do
-
-        begin
-          subject
-        rescue InjectedError
-        end
-
-        expect(queue_item.error).to match(/injected error/)
+      it "records the validation errors with formatting and indentation" do
+        subject
+        expect(queue_item.error).to match(/Error validating.*\n  injected error$/)
       end
     end
+
 
   end
 end
