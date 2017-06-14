01d0b107d33936ec19fd9752073179af7d2e07f5
Bryan Hockey <bhock@umich.edu>
2017-06-14 16:28:26 -0500

    Improve and decouple QueueItemBuilder return value
    Previously, this always returned a QueueItem instance, even
    if it failed to create on.  The caller--in our case, the
    controller--would need to obtain additional context in order to
    understand the returned value.
    
    Now, QueueItemBuilder returns both the QueueItem and a status,
    reducing the burden on callers, further decoupling them.
    
    Resolves PFDR-42

--- lib/queue_item_builder.rb
+++ lib/queue_item_builder.rb
@@ -5,12 +5,19 @@ class QueueItemBuilder
   end
 
   def create(request)
+    duplicate = QueueItem.where(bag: request, status: [:pending, :done]).first
+    unless duplicate.nil?
+      return :duplicate, duplicate
+    end
+
     queue_item = QueueItem.new(bag: request)
     if queue_item.valid?
       queue_item.save!
       BagMoveJob.perform_later(queue_item, request.upload_path, storage_location(request))
+      return :created, queue_item
+    else
+      return :invalid, queue_item
     end
-    return queue_item
   end
 
   private
