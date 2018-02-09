a86ff2c942349a4000f429b6fcb849eeab7e4ab4
Aaron Elkiss <aelkiss@umich.edu>
2018-02-09 15:47:06 -0500

    Fixes for bugs introduced in #30 / PFDR-83: fix argument parsing, remove empty spec

--- lib/chipmunk_cli.rb
+++ lib/chipmunk_cli.rb
@@ -3,13 +3,14 @@ require 'active_support/core_ext/hash'
 require 'yaml'
 require_relative './chipmunk_client'
 require_relative './uploader'
+require 'pry'
 
 class ChipmunkCLI
   def initialize(args)
-    parse_options
+    parse_options(args)
     load_default_config
 
-    @config = @config.symbolize_keys.slice(:uri,:api_key)
+    @config = @config.symbolize_keys.slice(:url,:api_key)
     config[:api_key] = ENV["CHIPMUNK_API_KEY"] if ENV["CHIPMUNK_API_KEY"]
 
     @client = ChipmunkClient.new(**config)
@@ -27,18 +28,20 @@ class ChipmunkCLI
 
   attr_reader :client, :config, :bag_paths
 
-  def parse_options
+  USAGE = "Usage: #{$PROGRAM_NAME} [options] /path/to/bag1 /path/to/bag2 ..."
+
+  def parse_options(args)
     OptionParser.new do |opts|
-      opts.banner = "Usage: #{$PROGRAM_NAME} [options] /path/to/bag1 /path/to/bag2 ..."
+      opts.banner = USAGE
 
       opts.on("-c", "--config", "Configuration file") do |c|
         @config = YAML.load(File.read(c))
       end
-    end
+    end.parse!(args)
 
-    raise ArgumentError, "Usage: #{$PROGRAM_NAME} " if ARGV.empty?
+    raise ArgumentError, USAGE if args.empty?
 
-    @bag_paths = ARGV
+    @bag_paths = args
   end
 
   def load_default_config
